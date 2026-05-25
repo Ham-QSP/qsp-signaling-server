74431dcbb4aefee68e1a6260ff9ec3fc8c5ac18f
Florian <76635263+f4fez@users.noreply.github.com>
2026-05-25 22:41:57 +0200

    Setup liquibase

--- src/main/kotlin/fr/f4fez/signaling/configuration/SecurityConfiguration.kt
+++ src/main/kotlin/fr/f4fez/signaling/configuration/SecurityConfiguration.kt
@@ -6,7 +6,6 @@ import org.springframework.http.HttpMethod
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
 import org.springframework.security.web.server.SecurityWebFilterChain
 
 
